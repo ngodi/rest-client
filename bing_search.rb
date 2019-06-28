3ca9d57398ff4917bf3d8cce035e68bd4b3eafec
ngodi <albertngodi@gmail.com>
2019-06-28 07:11:02 +0100

    initial commit

---
+++ bing_search.rb
@@ -0,0 +1,49 @@
+require 'rest-client'
+
+class Bing
+  attr_accessor :search_query, :search_results
+
+  URL = "https://www.bing.com/search"
+
+  def initialize(search_query)
+    @search_query = earch_query
+  end
+
+  def remove_spaces
+    @search_query.gsub!(/\s/, '+')
+  end
+
+  def search
+    remove_spaces
+    @search_results = RestClient.get URL, {params: {q: @search_query}}
+  end
+
+  def display_search_results
+    p search_results.code
+    p search_results.cookies
+    p search_results.headers
+    p search_results.body
+  end
+    def display_response_cookies
+        p @search_results.cookies
+    end
+
+  def display_response_code
+    p @search_results.code
+  end
+
+  
+  def display_response_headers
+    p @search_results.headers
+  end
+
+  def display_response_body
+    p @search_results.body
+  end
+end
+
+# puts "Please enter the search string:"
+# search_string = gets.chomp
+# bing_search = Bing.new(search_string)
+# bing_search.conduct_search
+# bing_search.display_search_results
